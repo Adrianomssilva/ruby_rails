7ae20e90a11ae43c0051e726bab3f6b085cf66ca
devadrsilva <adriano.mssilva1@gmail.com>
2023-07-24 11:40:08 -0300

    done idade.rb

--- idade.rb
+++ idade.rb
@@ -9,13 +9,21 @@ puts "[7] Julho [8] Agosto [9] Setembro"
 puts "[10] Outubro [11] Novembro [12] Dezembro"
 
 mes_de_nascimento = gets.to_i
-
-
-
-
-
-
 idade_mes = Time.new.month
+
+if (mes_de_nascimento >= idade_mes)
+   idade = (Time.new.year - ano_de_nascimento) - 1
+   
+ puts "Você nasceu em #{ano_de_nascimento} e tem #{idade} anos"
+else
+   idade = Time.new.year - ano_de_nascimento
+ puts "Você nasceu em #{ano_de_nascimento} e tem #{idade} anos"
+   
+end
+
+
+
+
 
 # idade = Time.new.year - ano_de_nascimento
 
