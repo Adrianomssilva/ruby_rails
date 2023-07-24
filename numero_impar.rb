6fdb1619bc1d955afaa0b653b65dfcc0c3db7376
devadrsilva <adriano.mssilva1@gmail.com>
2023-07-24 15:36:03 -0300

    final número_impar.rb

--- numero_impar.rb
+++ numero_impar.rb
@@ -1,20 +1,19 @@
 puts " Bem vindo a Máquina de números impares"
 puts " Digite dois números e a máquina vai te dar os números ímpares do intevarlo"
-
+ loop do
   puts "Digite o primeiro número: "
-num1 = gets.to_i 
-loop do
-puts "Digite o segundo número: "
-num2 = gets.to_i
-
-if num2 < num1
-puts "ERRRO: Digite um valor maior que o primeiro"   
-   else
-      break
-end
-end
-
-(num1..num2).each do |number|
-   puts numero if numero.odd?
+   num1 = gets.to_i 
+   puts "Digite o segundo número: "
+   num2 = gets.to_i
+   if num2<num1
+   puts "Error: O segundo valor deve ser maior que o primeiro"
    
+   else
+   puts "=========Imprimindo========="
+   (num1..num2).each do |number|
+   puts number if number.odd?
+   
+      end
+   break
+   end
 end
