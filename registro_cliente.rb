e1e04aff6604dc1a0698509097b87847a16977d0
devadrsilva <adriano.mssilva1@gmail.com>
2023-07-24 15:11:26 -0300

    primeira versão final registro_cliente.rb

--- registro_cliente.rb
+++ registro_cliente.rb
@@ -4,4 +4,21 @@ clientes = {
    2 => {nome: "Bianca", data_de_cadastro: "12/03/2021", cidade: "Central-Ba"},
    3 => {nome: "Maria", data_de_cadastro: "08/02/2022", cidade: "Riachão-Ba"}}
 
-   
+puts "Digite o cód de cliente"
+cod_cliente = gets.to_i
+
+puts "Procurando cliente..."
+
+sleep 3
+
+cliente = clientes[cod_cliente]
+
+if cliente != nil
+   puts "Nome: #{cliente[:nome]}"
+   puts "Data de Cadastro: #{cliente[:data_de_cadastro]}"
+   puts "Cidade: #{cliente[:cidade]}"
+else
+   puts "Cliente não encontrado"
+end
+
+puts "Programa encerrado!"
