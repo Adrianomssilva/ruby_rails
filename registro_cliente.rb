9030602be2e7c6f0f88f7f1c63a73a211feb2e48
devadrsilva <adriano.mssilva1@gmail.com>
2023-07-24 15:05:23 -0300

    base de clientes

---
+++ registro_cliente.rb
@@ -0,0 +1,7 @@
+
+clientes = {
+   1 => {nome: "Adriano", data_de_cadastro: "10/10/2020", cidade: "Salvador-Ba"},
+   2 => {nome: "Bianca", data_de_cadastro: "12/03/2021", cidade: "Central-Ba"},
+   3 => {nome: "Maria", data_de_cadastro: "08/02/2022", cidade: "Riachão-Ba"}}
+
+   
