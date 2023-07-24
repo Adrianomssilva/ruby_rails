78b6d975e497e19897d14a0e1b7269d1d3165886
devadrsilva <adriano.mssilva1@gmail.com>
2023-07-24 11:36:07 -0300

    mes add

--- idade.rb
+++ idade.rb
@@ -1,6 +1,22 @@
+require "date"
 puts "Qual ano você nasceu? "
 ano_de_nascimento = gets.to_i
 
-idade = Time.new.year - ano_de_nascimento
+puts "Qual seu mês de nascimento? "
+puts "[1] Janeiro [2] Fevereiro [3] Março"
+puts "[4] Abril [5] Maio [6] Junho"
+puts "[7] Julho [8] Agosto [9] Setembro"
+puts "[10] Outubro [11] Novembro [12] Dezembro"
 
-puts "Você nasceu no #{ano_de_nascimento} e tem #{idade} anos"
+mes_de_nascimento = gets.to_i
+
+
+
+
+
+
+idade_mes = Time.new.month
+
+# idade = Time.new.year - ano_de_nascimento
+
+# puts "Você nasceu em #{ano_de_nascimento} e tem #{idade} anos"
